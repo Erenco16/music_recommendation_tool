a06d818f3dffbbf7d9bd12c068b707b1bfcaca09
Erenco19 <erenbasaran50@gmail.com>
2025-03-02 18:36:19 +0000

    cors for the /recommend endpoint

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
-from app import create_app
+from flask import Flask, jsonify
+from flask_cors import CORS
 
-app = create_app()
+app = Flask(__name__)
+CORS(app, supports_credentials=True)  # Allow all origins
+
+@app.route("/recommend", methods=["GET", "OPTIONS"])
+def recommend():
+    return jsonify({"message": "CORS is working!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
